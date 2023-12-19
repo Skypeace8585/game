d256f1f5bb2a89669c88b808e0d08ab373644242
Skypeace8585 <i.takahashi.sys22@morijyobi.ac.jp>
2023-12-19 14:16:45 +0900

    はなことたろうを配置した

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -8,12 +8,19 @@ class MainScene extends Phaser.Scene {
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('back', 'assets/background.png');
+        this.load.image('hanako', 'assets/hanako.png');
+        this.load.image('taro', 'assets/taro.png');
     }
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(D_WIDTH/2, D_HEIGHT/2, 'back');
+        const hanako = this.physics.add.sprite(750, 400, 'hanako');
+        const taro = this.physics.add.sprite(50, 50, 'taro');
+
+        this.hanako=hanako;
+        this.taro=taro;
     }
     
   // 毎フレーム実行される繰り返し処理
